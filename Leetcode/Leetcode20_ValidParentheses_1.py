5b71bf081fac25d17180b8c3a356c9f614d65f68
MsClaire <ws771053840@126.com>
2019-08-05 08:03:57 +0800

    Leetcode20ValidParentheses

---
+++ Leetcode/Leetcode20_ValidParentheses_1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dict = {'{':'}','[':']','(':')'}
+        for i in s:
+            if i in dict:
+                stack.append(i)
+            elif len(stack)==0 or dict[stack.pop()] != i:
+                return False
+        return len(stack)==0
